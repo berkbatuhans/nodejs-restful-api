b69291e461ad02a45a1aa0b2d5e9430ea6618997
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2020-03-29 02:35:58 +0300

    Routes Added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import routes from './src/routes/nodeJSRoutes';
 
 const app = express();
 const PORT = 4000;
+
+routes(app);
 
 app.get('/', (request, response) => {
     response.send(`Node and express server running on port ${PORT}`)
 });
 
 app.listen(PORT, () => {
-    console.log(`Your server is running on port ${PORT}`)
+    console.log(`Your server is running on port ${PORT}`);
 });
