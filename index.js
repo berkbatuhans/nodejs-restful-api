a5a26c09ba85eacc812ccd49ba742b76d8448692
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2020-03-29 02:53:38 +0300

    Mongo Database Connection

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
 import routes from './src/routes/nodeJSRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
+
+mongoose.Promise = globel.Promise;
+
+//mongose connection
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser = true,
+    useUnifiedTopology: true
+});
+
+//bodyparser setup
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 routes(app);
 
