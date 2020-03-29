cc6cc5220fc63573089031012af696202f3f19f0
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2020-03-29 03:18:14 +0300

    Create all items GET endpoint

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,16 +1,17 @@
-import { addNewContact } from '../controllers/apiController';
+import {
+    addNewContact,
+    getContacts
+} from '../controllers/apiController';
 
 
 const routes = (app) => {
     app.route('/contact')
-        .get((req, res, next ) => {
+        .get((req, res, next) => {
             //middleware 
             console.log(`Request from: ${req.originalUrl}`)
             console.log(`Request type: ${req.method}`)
             next();
-        }, (req, res, next) => {
-            res.send('GET request successful')
-        })
+        }, getContacts)
 
         .post(addNewContact);
 
