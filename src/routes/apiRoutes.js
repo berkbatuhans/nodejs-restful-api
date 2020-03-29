25aacef353e1a812c56a20ed1d944c345afdde6f
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2020-03-29 03:13:04 +0300

    Create POST endpoint

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,3 +1,6 @@
+import { addNewContact } from '../controllers/apiController';
+
+
 const routes = (app) => {
     app.route('/contact')
         .get((req, res, next ) => {
@@ -9,8 +12,7 @@ const routes = (app) => {
             res.send('GET request successful')
         })
 
-        .post((req, res) =>
-            res.send('POST request successful'));
+        .post(addNewContact);
 
     app.route('/contact/:contactID')
         .put((req, res) =>
